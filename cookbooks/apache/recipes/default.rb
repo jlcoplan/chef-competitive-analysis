07c813f33ea5852916dcd6d22df0abe99e3313da
chefadmin <root@centos7.localdomain>
2017-12-01 10:16:09 -0500

    added motd and changed apache default for node attributes

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+if node['platform_family'] == "rhel"
+	package  = "httpd"
+
+elsif node['platform_family'] == "debian"
+	package = "apache2"
+
+end
+
+package 'httpd' do
+        package_name package
+        action install
+
+end
+
+
+service 'httpd' do
+        service_name 'httpd'
+        action [:start, :enable]
+
+
+end
